4af969ede846565ef5232519bfeedbe62e0f911a
travispbuckley <travispbuckley@lewisu.edu>
2018-09-05 20:31:39 -0500

    create unit test for square function

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -4,4 +4,16 @@ const utils = require('./utils');
 it('should add two numbers', () => {
     // test code goes here.
     var results = utils.add(33, 11);
+    
+    if (results !== 44) {
+        throw new Error(`Expected 44, but got ${results}`);
+    }
 });
+
+it('should square a number', () => {
+    var results = utils.square(4);
+
+    if (results !== 16) {
+        throw new Error(`Expected 16, but got ${results}`);
+    }
+})
