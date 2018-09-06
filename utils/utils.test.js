a9846a6892620af1c92fb4d412d5ca3fe0dcaa84
travispbuckley <travispbuckley@lewisu.edu>
2018-09-05 20:57:09 -0500

    add expect testing module and write test for setName

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,3 +1,5 @@
+const expect = require('expect');
+
 const utils = require('./utils');
 
 // first paramter is string that describes what the test is doing. BDD
@@ -5,15 +7,24 @@ it('should add two numbers', () => {
     // test code goes here.
     var results = utils.add(33, 11);
     
-    if (results !== 44) {
-        throw new Error(`Expected 44, but got ${results}`);
-    }
+    expect(results).toBe(44).toBeA('number');
 });
 
 it('should square a number', () => {
     var results = utils.square(4);
 
-    if (results !== 16) {
-        throw new Error(`Expected 16, but got ${results}`);
-    }
-})
+    expect(results).toBe(16).toBeA('number');
+});
+
+it('should set first and last names are set', () => {
+    var user = {
+        occupation: 'Developer',
+        age: 26
+    };
+
+    var results = utils.setName(user, 'Travis Buckley');
+
+    expect(results.firstName).toBe('Travis').toBeA('string');
+    expect(results.lastName).toBe('Buckley').toBeA('string');
+    expect(results).toBeAn('object');
+});
