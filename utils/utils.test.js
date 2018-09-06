7b700db1dfccd988e9318fb4521fad39416c04df
travispbuckley <travispbuckley@lewisu.edu>
2018-09-05 20:26:17 -0500

    add test file/test and update package.json test script call

---
+++ utils/utils.test.js
@@ -0,0 +1,7 @@
+const utils = require('./utils');
+
+// first paramter is string that describes what the test is doing. BDD
+it('should add two numbers', () => {
+    // test code goes here.
+    var results = utils.add(33, 11);
+});
