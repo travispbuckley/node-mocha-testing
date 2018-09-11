4d8ca3bd3085adb6097a5c9bbb4cca14361aba3a
travispbuckley <travispbuckley@lewisu.edu>
2018-09-10 21:49:54 -0500

    add tests for server app

--- server/server.js
+++ server/server.js
@@ -3,7 +3,25 @@ const express = require('express');
 var app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello world!');
+    res.status(404).send({
+        error: 'Page not found.',
+        name: 'Todo App v1.0'
+    });
 });
 
+app.get('/users', (req, res) => {
+    res.send([
+        {
+            name: 'Travis',
+            age: 26
+        }, 
+        {
+            name: 'Alexa',
+            age: 26
+        }
+    ]);
+})
+
 app.listen(3000);
+
+module.exports.app = app;
