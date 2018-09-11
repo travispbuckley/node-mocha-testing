2d48c658341965cfa017fa5ce35a0cf96ddf31bc
travispbuckley <travispbuckley@lewisu.edu>
2018-09-10 21:57:01 -0500

    add describe blocks to better organize test cases

--- server/server.test.js
+++ server/server.test.js
@@ -3,29 +3,35 @@ const expect = require('expect');
 
 var app = require('./server').app;
 
-it('should return hello world response', (done) => {
-    // use done to tell mocha to wait since it's an async request
-    request(app)
-      .get('/')
-      .expect(404)
-      .expect((res) => {
-        expect(res.body).toInclude({
-            error: 'Page not found.'
+describe('Server', () => {
+    describe('#GET /', () => {
+        it('should return hello world response', (done) => {
+            // use done to tell mocha to wait since it's an async request
+            request(app)
+              .get('/')
+              .expect(404)
+              .expect((res) => {
+                expect(res.body).toInclude({
+                    error: 'Page not found.'
+                });
+              }) 
+              .end(done);
         });
-      }) 
-      .end(done);
-});
+    });
 
-it('should return an array of users', (done) => {
-    // assert there is a 200 status response and that I exist inside the users array
-    request(app)
-      .get('/users')
-      .expect(200)
-      .expect((res) => {
-          expect(res.body).toInclude({
-              name: 'Travis',
-              age: 26
-          });
-      })
-      .end(done);
+    describe('#GET /users', () => {
+        it('should return an array of users', (done) => {
+            // assert there is a 200 status response and that I exist inside the users array
+            request(app)
+            .get('/users')
+            .expect(200)
+            .expect((res) => {
+                expect(res.body).toInclude({
+                    name: 'Travis',
+                    age: 26
+                });
+            })
+            .end(done);
+        });
+    });
 });
--- utils/utils.test.js
+++ utils/utils.test.js
@@ -2,34 +2,40 @@ const expect = require('expect');
 
 const utils = require('./utils');
 
-// first paramter is string that describes what the test is doing. BDD
-it('should add two numbers', () => {
-    // test code goes here.
-    var results = utils.add(33, 11);
-    
-    expect(results).toBe(44).toBeA('number');
-});
+describe('Utils', () => {
 
-// for async testing, can pass the done function and call below to tell the test not to be done until it reaches that point in the code.
-it('should async add two numbers', (done) => {
-    utils.asyncAdd(4, 3, (sum) => {
-        expect(sum).toBe(7).toBeA('number');
-        done();
+    describe('#add', () => {
+        it('should add two numbers', () => {
+            // test code goes here.
+            var results = utils.add(33, 11);
+            
+            expect(results).toBe(44).toBeA('number');
+        });
+        
+        // for async testing, can pass the done function and call below to tell the test not to be done until it reaches that point in the code.
+        it('should async add two numbers', (done) => {
+            utils.asyncAdd(4, 3, (sum) => {
+                expect(sum).toBe(7).toBeA('number');
+                done();
+            }); 
+        });
+    });
+    
+    it('should square a number', () => {
+        var results = utils.square(4);
+    
+        expect(results).toBe(16).toBeA('number');
+    });
+    
+    it('should async square a number', (done) => {
+        utils.asyncSquare(4, (square) => {
+            expect(square).toBe(16);
+            done();
+        });
     }); 
 });
+// first paramter is string that describes what the test is doing. BDD
 
-it('should square a number', () => {
-    var results = utils.square(4);
-
-    expect(results).toBe(16).toBeA('number');
-});
-
-it('should async square a number', (done) => {
-    utils.asyncSquare(4, (square) => {
-        expect(square).toBe(16);
-        done();
-    });
-});
 
 it('should set first and last names are set', () => {
     var user = {
